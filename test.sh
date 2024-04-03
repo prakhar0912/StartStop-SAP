22a02c95b1af3af456b6c3b3c0c34700fb57b4c9
Prakhar Soni <prakhar0912@gmail.com>
2024-04-03 14:05:54 +0530

    arst

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 pbrun -u d35adm -h pepldr03100.pi.pvt SHELL <<"ENDPBRUN"
-stopsap j2ee
-sapcontrol -nr 00 -function StopService
-sapcontrol -nr 01 -function StopService
-cleanipc 00 remove all
-cleanipc 01 remove all
+a = "COol bro"
+echo $a
 ENDPBRUN
