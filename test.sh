de222d617799ec1af5399004e6302e2577a05110
Prakhar Soni <prakhar0912@gmail.com>
2024-04-03 12:27:12 +0530

    arst

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
 pbrun -u d35adm -h pepldr03100.pi.pvt SHELL <<"ENDPBRUN"
+stopsap j2ee
+sapcontrol -nr 00 -function StopService
+sapcontrol -nr 01 -function StopService
+cleanipc 00 remove all
+cleanipc 01 remove all
+ENDPBRUN
